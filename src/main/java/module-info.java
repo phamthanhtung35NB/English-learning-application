f0cc8f8e57620292c704f6e4e5b16c4fa28a975a
Phạm Thanh Tùng <tungpham010203@gmail.com>
2023-09-20 22:13:40 +0700

    so khai

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.baitaplon {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.mediaEmpty;
+
+
+    opens com.example.baitaplon to javafx.fxml;
+    exports com.example.baitaplon;
+}
