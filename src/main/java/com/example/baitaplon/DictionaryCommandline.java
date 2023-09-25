b9319c7e61cff364cb6ca6de732bfccfb6c79063
Phạm Thanh Tùng <tungpham010203@gmail.com>
2023-09-26 00:38:33 +0700

    add them cac class so khai de lam viec

---
+++ src/main/java/com/example/baitaplon/DictionaryCommandline.java
@@ -0,0 +1,19 @@
+package com.example.baitaplon;
+
+public class DictionaryCommandline {
+    /**
+     * hiển thị kết quả danh sách dữ liệu từ vựng trên màn
+     * hình theo thứ tự alphabet.
+      */
+    public void showAllWords(){
+
+    }
+
+    /**
+     * có chức năng gọi hàm insertFromCommandline() và
+     * showAllWords().
+     */
+    public void dictionaryBasic(){
+
+    }
+}
---
+++ src/main/java/com/example/baitaplon/Word.java
@@ -0,0 +1,27 @@
+package com.example.baitaplon;
+
+public class Word {
+    String word_target; //tieng anh
+    String word_explain; //nghia tieng viet
+
+    public Word(String word_target, String word_explain) {
+        this.word_target = word_target;
+        this.word_explain = word_explain;
+    }
+
+    public String getWord_target() {
+        return word_target;
+    }
+
+    public String getWord_explain() {
+        return word_explain;
+    }
+
+    public void setWord_target(String word_target) {
+        this.word_target = word_target;
+    }
+
+    public void setWord_explain(String word_explain) {
+        this.word_explain = word_explain;
+    }
+}
