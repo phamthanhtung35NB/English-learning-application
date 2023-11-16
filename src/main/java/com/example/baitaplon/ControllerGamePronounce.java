6e76039f174e00423dbb953d3e88c2a14d27154c
Phạm Thanh Tùng <tungpham010203@gmail.com>
2023-11-17 03:43:26 +0700

    bat dau giao dien game + game doc tu

--- src/main/java/com/example/baitaplon/ControllerGamePronounce.java
+++ src/main/java/com/example/baitaplon/ControllerGamePronounce.java
@@ -19,14 +19,12 @@ public class ControllerGamePronounce extends Application {
     Label wordsReadLabel;
     @FXML
     private void startRecording() {
-        // Mở mic hoặc thực hiện các thao tác cần thiết khi bắt đầu thu âm
         System.out.println("Recording started");
     }
     private void startRecordingLoad() {
-        // Gọi hàm micString để thu âm và kiểm tra phát âm
+
         String result = micString();
 
-        // Kiểm tra kết quả và xử lý logic của game
         if ("hello".equalsIgnoreCase(result.trim())) {
             System.out.println("Correct pronunciation! You win!");
         } else {
@@ -35,18 +33,11 @@ public class ControllerGamePronounce extends Application {
     }
 
     private void stopRecording() {
-        // Đóng mic hoặc thực hiện các thao tác cần thiết khi dừng thu âm
         System.out.println("Recording stopped");
     }
 
-    // Hàm micString để thu âm giọng nói và trả về kết quả kiểu String
-    private String micString() {
-        // Thêm mã nguồn để ghi âm và chuyển đổi giọng nói thành văn bản ở đây
-        // Sử dụng thư viện Java Speech API hoặc thư viện thu âm của bạn
-        // ...
 
-        // Đoạn mã sau đây chỉ là giả định, bạn cần thêm mã nguồn thực tế
-        // để ghi âm và chuyển đổi giọng nói thành văn bản
+    private String micString() {
         return "hello";
     }
     @FXML
